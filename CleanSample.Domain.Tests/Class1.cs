557fd5d8ce82aeece63d1460678c5387a7e765c8
NicoPaez <nicopaez@10pines.com>
2017-11-17 10:40:40 -0300

    Initial solution structure

---
+++ CleanSample.Domain.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanSample.Domain.Tests
+{
+    [TestFixture]
+    public class FooTest
+    {
+        [Test]
+        public void ShouldDoFoo()
+        {
+            Assert.Pass();
+        }
+    }
+}
